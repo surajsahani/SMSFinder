e3b0f827dd0105c222f5c0bc2bbf9b76609cf994
suraj <surajkumarsahani1997@gmail.com>
2021-10-14 09:55:47 +0530

    completed function

--- app/src/main/java/com/martialcoder/smsfinder/MainActivity.kt
+++ app/src/main/java/com/martialcoder/smsfinder/MainActivity.kt
@@ -1,18 +1,118 @@
 package com.martialcoder.smsfinder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.martialcoder.smsfinder.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , InterfacePresenter.View {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
-    private lateinit var mainPresenterImp: InterfacePresenter
+    private lateinit var PermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var presenter: Persenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        presenter = Persenter(this, this)
+        UI(binding.root, this)
+
+        PermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+                if (granted) {
+                    presenter.fetch(
+                        binding.mobileNumber.text.toString(),
+                        binding.daysCount.text.toString()
+                    )
+                } else {
+                    showToast("SMS PErmisssion")
+                    return@registerForActivityResult
+                }
+            }
+
+        binding.mobileNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.daysCount.isVisible)
+                    binding.daysCount.visibility = View.GONE
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+        binding.daysCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (binding.daysCount.isVisible)
+                    binding.daysCount.visibility = View.GONE
+            }
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+        binding.submit.setOnClickListener {
+            GetSMS()
+
+        }
+    }
+
+    private fun GetSMS() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_SMS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                presenter.fetch(
+                    binding.mobileNumber.text.toString(),
+                    binding.daysCount.text.toString()
+                )
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.READ_SMS) -> {
+                PermissionLauncher.launch(android.Manifest.permission.READ_SMS)
+            }
+            else -> {
+                PermissionLauncher.launch(android.Manifest.permission.READ_SMS)
+            }
+        }
+    }
+
+
+    override fun Mobile(message: String) {
+        showLongToast(message)
+    }
+
+    override fun Days(message: String) {
+        showToast(message)
+    }
+
+    override fun invalid(message: String) {
+        showToast(message)
+    }
+
+    override fun Sucess(message: String) {
+        binding.daysCount.visibility = View.VISIBLE
+        binding.daysCount.text= message
+        binding.daysCount.setTextColor(getColor(R.color.success))
+    }
+
+    override fun Failure(message: String) {
+        binding.daysCount.visibility =  View.VISIBLE
+        binding.daysCount.text = message
+        binding.daysCount.setTextColor(getColor(R.color.error))
     }
 }
--- app/src/main/java/com/martialcoder/smsfinder/util.kt
+++ app/src/main/java/com/martialcoder/smsfinder/util.kt
@@ -1,4 +1,53 @@
 package com.martialcoder.smsfinder
 
-class util {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
+
+fun Context.showToast(text: CharSequence) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+}
+
+fun Context.showLongToast(text: CharSequence) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
+
+
+fun UI(view: View, context: Context) {
+    if (view !is EditText) {
+        view.setOnTouchListener { view, motionEvent ->
+            hide(context)
+            false
+        }
+    }
+
+    if (view is ViewGroup) {
+        for (i in 0 until view.childCount) {
+            val innerView = view.getChildAt(i)
+            UI(innerView, context)
+        }
+    }
+}
+
+fun hide(context: Context) {
+    try {
+        (context as AppCompatActivity).window.setSoftInputMode((WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN))
+        if (context.currentFocus != null && context.currentFocus!!.windowToken != null) {
+            (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+                context.currentFocus!!.windowToken,
+                0
+            )
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
